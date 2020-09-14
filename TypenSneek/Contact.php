50e83d8310a6e66b145206213b5751367218fa9a
Alexander Riemersma <Alexandersr@live.nl>
2020-09-14 13:29:02 +0200

    alle paginas (met nav)

--- TypenSneek/Contact.php
+++ TypenSneek/Contact.php
@@ -66,7 +66,19 @@
             <p>Mocht u toch nog vragen hebben over onze cursus, aarzel dan niet.<br>
                 Vul het contactformulier in. Wij streven ernaar om u binnen twee werkdagen antwoord te geven.
             </p>
+
+            <br>
+            <div style="width: 350px;position: relative;"><iframe width="350" height="170" src="https://maps.google.com/maps?width=350&amp;height=120&amp;hl=en&amp;q=Pinksterbloem%2026+(Typen%20Sneek)&amp;ie=UTF8&amp;t=&amp;z=13&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+                <div style="position: absolute;width: 80%;bottom: 10px;left: 0;right: 0;margin-left: auto;margin-right: auto;color: #000;text-align: center;">
+                    <small style="line-height: 1.8;font-size: 2px;background: #fff;">
+                    </small></div><style>#gmap_canvas img{max-width:none!important;background:none!important}</style>
+            </div>
+            <br />
+
         </div>
+
+
+
         <div class="col-md-8">
             <form action="/post" method="post">
                 <input class="form-control" name="name" placeholder="Naam..." /><br />
