4fb0e5bdf46036715f84af2a96924a6e49d046a1
Super <kelvinpolman@ziggo.nl>
2020-09-14 10:23:52 +0200

    Over jou aangepast naar over ons

--- TypenSneek/index.php
+++ TypenSneek/index.php
@@ -23,7 +23,7 @@
               </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Over jou</a>
+          <a class="nav-link" href="#">Over ons</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Blindtypen</a>
