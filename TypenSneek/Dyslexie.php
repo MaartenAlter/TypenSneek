dd353d11838c17521ccf7ce5f1c23b57159f6fb3
Alexander Riemersma <Alexandersr@live.nl>
2020-09-14 13:08:27 +0200

    alle paginas (met nav)

--- TypenSneek/Dyslexie.php
+++ TypenSneek/Dyslexie.php
@@ -15,34 +15,34 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#">TypenSneek</a>
+        <a class="navbar-brand" href="index.php">TypenSneek</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Hoofdpagina
-                        <span class="sr-only">(current)</span>
-                    </a>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="OverOns.php">Over ons</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Over ons</a>
+                    <a class="nav-link" href="Blindtypen.php">Blindtypen</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Blindtypen</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Ervaringen</a>
+                    <a class="nav-link" href="Ervaringen.php">Ervaringen</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="Dyslexie.php">Dyslexie</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">De cursus</a>
+                    <a class="nav-link" href="DeCursus.php">De cursus</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="aanmelden.php">Aanmelden</a>
+                </li>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="Contact.php">Contact</a>
                 </li>
             </ul>
         </div>
