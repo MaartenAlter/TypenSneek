221b277e56dde012a4eb85119b229f6ae87562fd
Alexander Riemersma <Alexandersr@live.nl>
2020-09-16 10:35:51 +0200

    chapcha

--- TypenSneek/index.php
+++ TypenSneek/index.php
@@ -17,7 +17,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="#">TypenSneek</a>
+        <a class="navbar-brand" href="index.php">TypenSneek</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -136,7 +136,7 @@
                 8607 DV SNEEK<br>
                 0515 - 419424<br>
                 <br><br>
-                <button type="button" class="btn btn-outline-primary">Stuur een bericht</button>
+                <button type="button" class="btn btn-outline-primary" >Stuur een bericht</button>
 
 
             </div>
