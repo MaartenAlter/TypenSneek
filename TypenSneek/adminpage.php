698a609d93505dfe8a14f64e2989144b245deddf
MisterMaarten <maartenalter@outlook.com>
2020-09-21 15:17:07 +0200

    Ja

--- TypenSneek/adminpage.php
+++ TypenSneek/adminpage.php
@@ -103,13 +103,63 @@ echo " </form>";
 }
         
 
-
-
     if (isset($_GET['action']) && $_GET['action'] === 'delete') {
         $sql = "DELETE FROM gebruikers WHERE  ID=" .$_GET['id'];
         $result = mysqli_query($conn, $sql);
         header("Location: adminpage.php");
         }
+
+
+        $result = mysqli_query($conn,"SELECT * FROM gebruikers WHERE Aangemeld = 1 ");
+
+        echo "<br><br><h2> Geregistreerde gebruikers: </h2>";
+        echo "<table border='1'>
+        <tr>
+        <th>Voornaam</th>
+        <th>Achternaam</th>
+        <th>Email</th>
+        <th>Telefoonnummer</th>
+        <th>GeboorteDatum</th>
+        <th>Straat</th>
+        <th>Postcode</th>
+        <th>Woonplaats</th>
+        <th>School</th>
+        <th>Opmerking</th>
+        <th>Gemaakt op</th>
+        <th>Gebruikerstype</th>
+        <!-- <th>Gebruikersnaam</th> -->
+        <!--  <th>Wachtwoord</th> -->
+        
+        </tr>";
+        
+        while($row = mysqli_fetch_array($result))
+        {
+        echo "<form action='adminpage.php' method='POST'>";
+        echo "<tr>";
+        echo "<td>" . $row['Voornaam'] . "</td>";
+        echo "<td>" . $row['Achternaam'] . "</td>";
+        echo "<td>" . $row['Email'] . "</td>";
+        echo "<td>" . $row['Telefoonnummer'] . "</td>";
+        echo "<td>" . $row['GeboorteDatum'] . "</td>";
+        echo "<td>" . $row['Straat'] . "</td>";
+        echo "<td>" . $row['Postcode'] . "</td>";
+        echo "<td>" . $row['Woonplaats'] . "</td>";
+        echo "<td>" . $row['School'] . "</td>";
+        echo "<td>" . $row['Opmerking'] . "</td>";
+        echo "<td>" . $row['GemaaktOp'] . "</td>";
+        echo "<td>" . $row['GebruikerType'] . "</td>";
+        //echo "<td>" . "<input type='text' name='username'  id=" .$row['ID']."   >" . "</td>";
+        //echo "<td>" . "<input type='password' name='password' id=" .$row['ID']." >". "</td>";
+        echo "<td>" . "<a href='adminpage.php?action=delete&id=".$row['ID']."'onClick='return confirm(/Wilt u deze gebruiker wissen?/)'>verwijder</a>" . "</td>";
+        echo "</tr>";
+        
+        
+        }
+        echo "</table>";
+        echo " </form>";
+
+
+
 
 
     mysqli_close($conn);
