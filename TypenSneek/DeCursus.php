b9396b4ee0525c4e417bd2b4a20ae936ef192d24
MisterMaarten <maartenalter@outlook.com>
2020-09-28 09:52:25 +0200

    Cursus inplementatie

--- TypenSneek/DeCursus.php
+++ TypenSneek/DeCursus.php
@@ -1,154 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="stylecursus.css">
+  <script src="script.js" defer></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-<!--    external css -->
-    <link rel="stylesheet" href="css/footer.css"
-    <title>Hello, world!</title>
-
-    <script src="https://www.google.com/recaptcha/api.js"></script>
-    <script>
-    function onSubmit(token) {
-        document.getElementById("captcha").submit();
-    }
-</script>
+    <title>TypenSneek</title>
 </head>
-<nav class="navbar navbar-expand-lg navbar-light bg-primary purple shadow fixed-top mx-auto " >
-    <div class="container mx-auto ">
-        <a class="navbar-brand" href="index.php"><img src="img/Logo.png" width="auto" height="50" class="d-inline-block align-top" alt=""></a>
-        <button class="navbar-toggler mx-auto " type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon "></span>
-        </button>
-        <div class="collapse navbar-collapse mxt-4 pt-1" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto ">
-
-
-                <li class="nav-item mx-1 pt-2 text-center">
-                    <a class="nav-link bg-warning rounded-pill" href="Blindtypen.php">Blindtypen</a>
-                </li>
-                <li class="nav-item mx-1 pt-2 text-center">
-                    <a class="nav-link bg-success rounded-pill" href="Ervaringen.php">Ervaringen</a>
-                </li>
-                <li class="nav-item mx-1 pt-2 text-center">
-                    <a class="nav-link bg-danger rounded-pill" href="Dyslexie.php">Dyslexie</a>
-                </li>
-                <li class="nav-item mx-1 pt-2 text-center ">
-                    <a class="nav-link bg-success rounded-pill " href="DeCursus.php ">De cursus</a>
-                </li>
-                <li class="nav-item mx-1 pt-2 text-center">
-                    <a class="nav-link bg-warning rounded-pill"  href="aanmelden.php">Aanmelden</a>
-                </li>
-                <li class="nav-item  mx-1 pt-2 text-center">
-                    <a class="nav-link bg-danger rounded-pill" href="Contact.php">Contact</a>
-                </li><?php
-                if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-
-                    if($_SESSION["usertype"] == "admin"){
-                        echo "<li class='nav-item'>
-                            <a class='nav-link' href='adminpage.php'>Coach Pagina</a>
-                            </li>";
-
-                    }
-
-
-                }
-                if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-                    //header("location: login.php");
-                    //exit;
-
-                    echo "<form action='index.php' method='post'> <input type='submit' name='logout' value='Uitloggen' class='btn btn-primary'> </form> ";
-
-                    if(isset($_POST['logout'])){
-                        // Initialize the session
-                        session_start();
-
-                        // Unset all of the session variables
-                        $_SESSION = array();
-
-                        // Destroy the session.
-                        session_destroy();
-
-                        // Redirect to login page
-                        header("location: index.php");
-                        exit;
-                    }
-                }
-                ?>
-            </ul>
-        </div>
+<body >
+    <div class=>
+        <h2>Goedemorgen</h2>
     </div>
-</nav>
-
-
-<!--    footer-->
-<!-- Grid row -->
-<div class="footer" id="footer">
-
-    <!-- Grid column -->
-    <div class="col-md-12 mb-4">
-
-        <!--Footer-->
-        <footer class="page-footer blue text-center text-md-left mt-0">
-
-            <!--Footer Links-->
-            <div class="container-fluid">
-                <div class="row">
-
-                    <!--First column-->
-                    <div class="col-md-6">
-                        <h5 class="title mb-3">TypenSneek</h5>
-                        <p>               Pinksterbloem 26 <br>
-                            8607 DV SNEEK<br>
-                            0515 - 419424<br>
-                            <br><br> </p>
-                    </div>
-                    <!--/.First column-->
-
-                    <!--Second column-->
-                    <div class="col-md-3">
-                        <h5 class="list-unstyled quick-links">Links</h5>
-                        <ul>
-                            <li><a href=""><i class="fa fa-angle-double-right"></i>Aanmelden</a></li>
-                            <li><a href=""><i class="fa fa-angle-double-right"></i>Blindtypen</a></li>
-                            <li><a href=""><i class="fa fa-angle-double-right"></i>Dyslexie</a></li>
-                            <li><a href=""><i class="fa fa-angle-double-right"></i>Over ons</a></li>
-
-                        </ul>
-                    </div>
-
-                    <div class="col-md-3">
-                        <h5 class="list-unstyled quick-links"><br></h5>
-                        <ul>
-
-                            <li><a href=""><i class="fa fa-angle-double-right"></i>Ervaringen</a></li>
-                            <li><a href=""><i class="fa fa-angle-double-right"></i>De cursus</a></li>
-                            <li><a href=""><i class="fa fa-angle-double-right"></i>Contact</a></li>
-                        </ul>
-                    </div>
-                    <!--/.Second column-->
-                </div>
-            </div>
-            <!--/.Footer Links-->
-
-            <!--Copyright-->
-            <div class="footer-copyright">
-                <div class="container-fluid">
-                    © 2020 Copyright: <a href="index.php"> TypenSneek.nl </a>
-
-                </div>
-            </div>
-            <!--/.Copyright-->
-
-        </footer>
-        <!--/.Footer-->
-
-    </div>
-    <!-- Grid column -->
-</div>
-<!-- Grid row -->
-<!-- ./Footer -->
+  <div class="cursus">
+    <div class="quote-display" id="quoteDisplay"></div>
+    <textarea id="quoteInput" class="quote-input" autofocus></textarea>
+  </div>
+</body>
+</html>
